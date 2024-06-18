9b82320e702a82dcf39937da86b32b4468976b3c
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-06-18 18:58:06 -0400

    add title attribute after animation

---
+++ script.js
@@ -0,0 +1,10 @@
+// ADDING TITLE ATTRIBUTES AFTER INTRO ANIMATION
+setTimeout(() => {
+  const icons = document.querySelectorAll('.social-media-icons i');
+
+  icons[0].setAttribute('title', 'Facebook');
+  icons[1].setAttribute('title', 'Instagram');
+  icons[2].setAttribute('title', 'Twitter [X]');
+  icons[3].setAttribute('title', 'Youtube');
+  icons[4].setAttribute('title', 'Github');
+}, 5000);
