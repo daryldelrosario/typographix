0a8926003fc73c4ea94eb5b424f76d4e3665019b
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-06-20 16:36:36 -0400

    setup dark toggle html and script

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ setTimeout(() => {
   icons[3].setAttribute('title', 'Youtube');
   icons[4].setAttribute('title', 'Github');
 }, 3000);
+
+// DARK MODE EXERCISE
+const themeSwitcher = document.getElementById("theme-switcher");
+
+// FUNCTION for darkmode
+function darkMode() {
+  themeSwitcher.children[0].textContent = "Dark Mode";
+  themeSwitcher.children[1].classList.replace("fa-sun", "fa-moon");
+}
+
+// FUNCTION for lightmode
+function lightMode() {
+  themeSwitcher.children[0].textContent = "Light Mode";
+  themeSwitcher.children[1].classList.replace("fa-moon", "fa-sun");
+}
+
+// FUNCTION to switch the themese 
+function switchTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  if(!currentTheme || currentTheme === 'light') {
+    DocumentTimeline.documentElement.setAttribute("data-theme", "dark");
+    darkMode();
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    lightMode();
+  }
+}
+
+// ADDEVENT LISTENERS
+themeSwitcher.addEvenetListener("click", switchTheme);
