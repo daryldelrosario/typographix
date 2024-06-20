d067d4d12e65ec7a7f701b962f093a8d816bcba7
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-06-20 16:53:35 -0400

    setup local storage for theme

--- script.js
+++ script.js
@@ -29,12 +29,17 @@ function switchTheme() {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   if(!currentTheme || currentTheme === 'light') {
     document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
     darkMode();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
     lightMode();
   }
 }
 
 // ADDEVENT LISTENERS
 themeSwitcher.addEventListener("click", switchTheme);
+
+// LOCAL STORAGE EXERCISE 
+const localStorageTheme = localStorage.getItem("theme");
