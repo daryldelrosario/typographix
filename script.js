b64b25ccb0da02193c55f1dd7f31cd2ca33c1032
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-06-26 16:23:35 -0400

    include script for navigation menu

--- script.js
+++ script.js
@@ -51,3 +51,17 @@ if(localStorageTheme) {
     lightMode();
   }
 }
+
+// NAVIGATION MENU
+const nav = document.getElementById("nav");
+const menuIcon = document.querySelector(".menu-icon");
+
+function toggleMenu() {
+  nav.classList.toggle("active");
+  menuIcon.classList.toggle("active");
+}
+
+function hideMenu() {
+  nav.classList.remove("active");
+  menuIcon.classList.remove("active");
+}
