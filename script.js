af1882ab4325ad0cf46fd5c50e4ee62f3b2a6999
Daryl del Rosario <daryldelrosario.is@gmail.com>
2024-09-14 13:12:17 -0400

    clean up javascript for them and icone switch

--- script.js
+++ script.js
@@ -12,17 +12,13 @@ setTimeout(() => {
 // DARK MODE EXERCISE
 const themeSwitcher = document.getElementById("theme-switcher");
 
-// FUNCTION for darkmode
-function darkMode() {
-  themeSwitcher.children[0].textContent = "Dark Mode";
-  themeSwitcher.children[1].classList.replace("fa-sun", "fa-moon");
+// FUNCTION for UPDATE THEME ICON & TEXT
+function updateThemeIcon(isDarkMode) {
+  themeSwitcher.children[0].textContent = isDarkMode ? 'Dark Mode' : 'Light Mode';
+  themeSwitcher.children[1].classList.replace(isDarkMode ? 'fa-sun': 'fa-moon', isDarkMode ? 'fa-moon': 'fa-sun');
 }
 
-// FUNCTION for lightmode
-function lightMode() {
-  themeSwitcher.children[0].textContent = "Light Mode";
-  themeSwitcher.children[1].classList.replace("fa-moon", "fa-sun");
-}
+
 
 // FUNCTION to switch the themese 
 function switchTheme() {
